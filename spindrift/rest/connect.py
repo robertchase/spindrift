a857dd398c49ec9d2010fb033697c0b363520592
bob chase <bob.chase@gmail.com>
2017-12-27 18:20:58 -0500

    add evaluate, debug and trace to rest.connect

--- spindrift/rest/connect.py
+++ spindrift/rest/connect.py
@@ -5,6 +5,7 @@ https://github.com/robertchase/spindrift/blob/master/LICENSE.txt
 '''
 import json
 from socket import gethostbyname
+import time
 import urllib.parse as urllib
 
 from spindrift.http import HTTPHandler
@@ -14,44 +15,127 @@ import logging
 log = logging.getLogger(__name__)
 
 
-def connect(network, timer, callback, url, method='GET', body=None, headers=None, is_json=True, timeout=5.0, wrapper=None, handler=None, **kwargs):
-    '''
-        Make an async rest connection, executing callback on completion
+def connect(
+            network,
+            timer,
+            callback,
+            url,
+            query=None,
+            method='GET',
+            body=None,
+            headers=None,
+            is_json=True,
+            is_form=False,
+            timeout=5.0,
+            wrapper=None,
+            evaluate=None,
+            handler=None,
+            debug=False,
+            trace=False,
+            **kwargs
+        ):
+    """ Make an async rest connection, executing callback on completion
 
         Parameters:
 
-            network - instance of spindrift.network.Network
-            timer - instance of spindrift.timer.Timer
+            network  - instance of spindrift.network.Network
+            timer    - instance of spindrift.timer.Timer
             callback - a callable expecting (rc, result), where rc=0 on success
-            url - full url of the resource being referenced
-            method - http method (default=GET)
-            body - http content (default=None)
-            headers - http headers as dict (default=None)
-            is_json - if True, successful result is json.loads-ed (default=True)
-            timeout - tolerable period of network inactivity in seconds (default=5.0)
-                      on timeout, callback is invoked with (1, 'timeout')
-            wrapper - if successful, wrap result in wrapper before callback (default=None)
-            handler - handler class for connection (default=None)
-                      subclass of ConnectHandler
-            kwargs - additional keyword args that might be useful in a ConnectHandler subclass
+            url      - full url of the resource being referenced
+                       can include query string
+            query    - optional query string
+                       if dict, urlencoded to string
+            method   - http method (default=GET)
+            body     - http content (default=None)
+            headers  - http headers as dict (default=None)
+            is_json  - if True, successful result is json.loads-ed
+                       (default=True)
+            is_form  - send content as applicaton/x-www-form-urlencoded
+            timeout  - tolerable period of network inactivity in seconds
+                       (default=5.0)
+                       on timeout, callback is invoked with (1, 'timeout')
+            wrapper  - callable for wrapping successful result
+                       called with result before callback
+                       (default=None)
+            handler  - handler class for connection (default=None)
+                       subclass of ConnectHandler
+            evaluate - callable for http response evaluation (default=None)
+                       returns result or raises Exception
+                       (see ConnectHandler.evaluate)
+            debug    - log debug messages on start/open/close
+            trace    - log debug sent and recv'd http data
+            kwargs   - additional keyword args that might be useful in a
+                       ConnectHandler subclass
 
         Notes:
 
             1. If body is a dict and method is GET, then the contents of dict are added
                to the query string and body is cleared.
-    '''
+    """
+    if query is not None:
+        if isinstance(query, dict):
+            query = urllib.urlencode(query)
+        url = '{}?{}'.format(url, query)
     p = URLParser(url)
-    return connect_parsed(network, timer, callback, url, p.host, p.address, p.port, p.path, p.query, p.is_ssl, method, headers, body, is_json, timeout, wrapper, handler, **kwargs)
+    return connect_parsed(network, timer, callback, url, p.host, p.address,
+                          p.port, p.path, p.query, p.is_ssl, method, headers,
+                          body, is_json, is_form, timeout, wrapper, handler,
+                          evaluate, debug, trace, **kwargs)
 
 
-def connect_parsed(network, timer, callback, url, host, address, port, path, query, is_ssl, method, headers, body, is_json, timeout, wrapper, handler, **kwargs):
-    c = ConnectContext(callback, timer, url, method, path, query, host, headers, body, is_json, timeout, wrapper, kwargs)
-    return network.add_connection(address, port, handler or ConnectHandler, c, is_ssl=is_ssl)
+def connect_parsed(
+            network,
+            timer,
+            callback,
+            url,
+            host,
+            address,
+            port,
+            path,
+            query,
+            is_ssl,
+            method,
+            headers,
+            body,
+            is_json,
+            is_form,
+            timeout,
+            wrapper,
+            handler,
+            evaluate,
+            debug,
+            trace,
+            **kwargs
+        ):
+    c = ConnectContext(callback, timer, url, method, path, query, host,
+                       headers, body, is_json, is_form, timeout, wrapper,
+                       evaluate, debug, trace, kwargs)
+    return network.add_connection(address, port, handler or ConnectHandler, c,
+                                  is_ssl=is_ssl)
 
 
 class ConnectContext(object):
 
-    def __init__(self, callback, timer, url, method, path, query, host, headers, body, is_json, timeout, wrapper, kwargs):
+    def __init__(
+                self,
+                callback,
+                timer,
+                url,
+                method,
+                path,
+                query,
+                host,
+                headers,
+                body,
+                is_json,
+                is_form,
+                timeout,
+                wrapper,
+                evaluate,
+                is_debug,
+                is_trace,
+                kwargs
+            ):
         self.callback = callback
         self.timer = timer
         self.url = url
@@ -62,9 +146,12 @@ class ConnectContext(object):
         self.headers = headers
         self.body = body
         self.is_json = is_json
-        self.is_verbose = False
+        self.is_form = is_form
         self.timeout = timeout
         self.wrapper = wrapper
+        self.evaluate = evaluate
+        self.is_debug = is_debug
+        self.is_trace = is_trace
         self.kwargs = kwargs
 
 
@@ -76,29 +163,56 @@ class ConnectHandler(HTTPHandler):
         self.is_done = False
         self.is_timeout = False
         self.setup()
+        self.check_kwargs()
         self.after_init()
 
-    def after_init(self):
+    def check_kwargs(self):
         kwargs = self.context.kwargs
         if len(kwargs) > 0:
-            raise TypeError('connect() received unexpected keyword argument(s): %s' % str(tuple(kwargs.keys())))
+            raise TypeError(
+                'connect() received unexpected keyword argument(s): %s' %
+                str(tuple(kwargs.keys()))
+            )
+
+    def after_init(self):
+        if self.context.is_debug:
+            log.debug(
+                'starting outbound connection, oid=%s: %s %s',
+                self.id,
+                self.context.method,
+                self.context.url + self.context.path
+            )
+
+    def _form(self, context):
+        if context.is_form:
+            if not context.headers:
+                context.headers = {}
+            context.headers['Content-Type'] = \
+                'application/x-www-form-urlencoded'
 
     def setup(self):
         context = self.context
 
-        if context.headers and context.headers.get('Content-Type') == 'application/x-www-form-urlencoded' and isinstance(context.body, dict):
-            context.body = urllib.urlencode(context.body)
+        self._form(context)
 
-        if context.method == 'GET' and isinstance(context.body, dict):
-            query = urllib.parse_qs(context.query)
-            query.update(context.body)
-            context.query = urllib.urlencode(context.body)
-            context.body = None
+        if isinstance(context.body, dict):
+            context_type = None
+            if context.headers:
+                context_type = context.headers.get('Content-Type')
+
+            if context_type == 'application/x-www-form-urlencoded':
+                context.body = urllib.urlencode(context.body)
+            elif context.method == 'GET':
+                query = urllib.parse_qs(context.query)
+                query.update(context.body)
+                context.query = urllib.urlencode(context.body)
+                context.body = None
 
         if context.path == '':
             context.path = '/'
 
-        context.path = context.path + ('?%s' % context.query if context.query else '')
+        if context.query:
+            context.path = context.path + '?' + context.query
 
         if isinstance(context.body, (dict, list, tuple, float, bool, int)):
             try:
@@ -108,11 +222,14 @@ class ConnectHandler(HTTPHandler):
             else:
                 if context.headers is None:
                     context.headers = {}
-                context.headers['Content-Type'] = 'application/json; charset=utf-8'
+                context.headers['Content-Type'] = \
+                    'application/json; charset=utf-8'
 
         if context.body is None:
             context.body = ''
-        self.timer = self.context.timer.add(self.on_timeout, self.context.timeout * 1000).start()
+        self.timer = self.context.timer.add(
+            self.on_timeout, self.context.timeout * 1000
+        ).start()
 
     def done(self, result, rc=0):
         if self.is_done:
@@ -122,8 +239,34 @@ class ConnectHandler(HTTPHandler):
         self.context.callback(rc, result)
         self.close('transaction complete')
 
+    def on_open(self):
+        if self.context.is_debug:
+            log.debug('open oid=%s: %s', self.id, self.full_address)
+
     def on_close(self, reason):
-        self.done(None)
+        if self.context.is_debug:
+            now = time.time()
+            msg = 'close oid=%s, reason=%s, opn=%.4f,' % (
+                self.id,
+                reason,
+                (self.t_open - self.t_init) if self.t_open else 0,
+            )
+            if self.is_ssl:
+                msg += ' rdy=%.4f,' % (
+                    (self.t_ready - self.t_init) if self.t_ready else 0,
+                )
+            msg += ' dat=%.4f, tot=%.4f, rx=%d, tx=%d' % (
+                (self.t_http_data - self.t_init) if self.t_http_data else 0,
+                now - self.t_init,
+                self.rx_count,
+                self.tx_count,
+            )
+            if self.is_ssl:
+                msg += ', ssl handshake=%s' % (
+                    'success' if self.t_ready else 'fail',
+                )
+            log.debug(msg)
+        self.done(reason)
 
     def on_failed_handshake(self, reason):
         log.warning('ssl error cid=%s: %s', self.id, reason)
@@ -148,16 +291,40 @@ class ConnectHandler(HTTPHandler):
         super(ConnectHandler, self).on_data(data)
 
     def evaluate(self):
+        """ evaluate http response document
+
+            examines http status code, raising an Exception if not in
+            the 200-299 range.
+
+            Return:
+
+                if method is HEAD, http_headers
+                otherwise http_content
+
+            Notes:
+
+                1. can be overridden by a subclass, or by specifying the
+                   evaluate argument to the connect function.
+        """
+        if self.context.method == 'HEAD':
+            result = self.http_headers
+        else:
+            result = self.http_content
         status = self.http_status_code
-        result = self.http_headers if self.context.method == 'HEAD' else self.http_content
-        if status < 200 or status >= 300:
-            return self.done(self.http_status_message if result == '' else result, 1)
+        if status < 200 or status > 299:
+            raise Exception(result or self.http_status_message)
         return result
 
     def on_http_data(self):
-        result = self.evaluate()
-        if self.is_done:
-            return
+
+        if self.context.is_trace:
+            log.debug('recv: %s', self.http_message)
+
+        try:
+            evaluate = self.context.evaluate or self.__class__.evaluate
+            result = evaluate(self)
+        except Exception as e:
+            return self.done(str(e), 1)
 
         if self.context.is_json and result is not None and len(result):
             try:
@@ -182,6 +349,19 @@ class ConnectHandler(HTTPHandler):
     def on_timeout(self):
         self.is_timeout = True
         self.done('timeout', 1)
+
+    def on_http_send(self, headers, content):
+        if self.context.is_trace:
+            log.debug('send: %s', headers)
+            if len(content):
+                log.debug('send: %s', content)
+
+
+def run(network, timer, command, sleep=100, max_iterations=100):
+    """ service network and timer until command.is_done is True """
+    while not command.is_done:
+        network.service(timeout=sleep/1000.0, max_iterations=max_iterations)
+        timer.service()
 
 
 class URLParser(object):
