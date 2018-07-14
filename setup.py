5c09bc5cd1611c81c70db7248a45fcbb10c61d71
bob chase <${SETUP_GIT_EMAIL:-bob.chase@gmail.com}>
2018-07-14 08:31:32 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name='spindrift',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests']),
+    description='A rest framework',
+    long_description="""
+Documentation
+-------------
+    You can see the project and documentation at the `GitHub repo <https://github.com/robertchase/spindrift>`_
+    """,
+    author='Bob Chase',
+    url='https://github.com/robertchase/spindrift',
+    license='MIT',
+)
