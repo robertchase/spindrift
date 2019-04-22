12177b88618292039a57d4545bbf6fbe339c0d6d
bob chase <bob.chase@gmail.com>
2019-04-22 06:27:13 -0400

    improve docker handling

--- container.sh
+++ container.sh
@@ -4,4 +4,4 @@ NET=${TEST_NET:-test}
 MYSQL_HOST=${TEST_MYSQL:-mysql}
 IMAGE=${IMAGE:-spindrift-dev}
 
-docker run $OPT -it --rm -v=$DIR:/opt/git -w /opt/git/spindrift --net $NET -e MYSQL_HOST=$MYSQL_HOST --name spindrift $IMAGE "$@"
+docker run $OPT -it --rm -v=$DIR:/opt/git -w /opt/git/spindrift --net $NET -e PYTHONPATH=. -e MYSQL_HOST=$MYSQL_HOST --name spindrift $IMAGE "$@"
